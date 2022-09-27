9d34d9c7bba4e99af221747598dfd46b6510be7f
Lucas Pugliesi <ferreiraplucas@gmail.com>
2022-09-27 20:30:14 +0100

    Day 1

---
+++ day-1/band_name_generator.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator.')
+city = input('What is the name of the city you grew up in?\n')
+pet = input('What is the name of a pet?\n')
+band_name = city + " " + pet
+print('Your band name could be ' + band_name)
