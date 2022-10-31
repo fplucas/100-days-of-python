a7efefb9b1b957796a46d60f454af272d5f02581
Lucas Pugliesi <ferreiraplucas@gmail.com>
2022-10-31 22:14:55 +0000

    Day 20

---
+++ day-20/main.py
@@ -0,0 +1,27 @@
+import time
+from turtle import Screen, Turtle
+from snake import Snake
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My snake game')
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
