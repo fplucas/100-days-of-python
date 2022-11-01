deddca5122a3cbd88ffdf12d4d6c7624ca34f0d9
Lucas Pugliesi <ferreiraplucas@gmail.com>
2022-11-01 20:42:38 +0000

    Day 21

---
+++ day-21/main.py
@@ -0,0 +1,47 @@
+import time
+from turtle import Screen, Turtle
+
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My snake game')
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.hit_the_wall():
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
